1ffd12ae5772e40bf92bfbbe2499be4b7b475eb4
unknown <08250286.sce@rub.edu.bt>
2026-03-18 10:02:22 +0530

    Learning string commands for education

---
+++ Class demo/StringEx.py
@@ -0,0 +1,11 @@
+print("Hellow")
+print('Hellow')
+multipleLine = '''This is a string
+that spans multiple lines'''
+print(multipleLine)
+a = 1
+b = 2
+c = a + b
+print(type(c))
+c = str(c)
+print(type(c))
