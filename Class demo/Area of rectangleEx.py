9f8e52e80ca583ef8744c14e933984ab59c658ae
unknown <08250286.sce@rub.edu.bt>
2026-03-20 15:39:00 +0530

    Learning

---
+++ Class demo/Area of rectangleEx.py
@@ -0,0 +1,6 @@
+L = float (input ("Enter the length of the rectangle"));
+
+B = float (input ("Enter the length of the rectangle"));
+
+area = L * B
+print ("The length of the rectangle:" + str(area))
