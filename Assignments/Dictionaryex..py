21cdc16233ef60085485427b46fd30531c6615df
unknown <08250286.sce@rub.edu.bt>
2026-04-03 16:07:40 +0530

    Learning Dictionary

--- Assignments/Dictionaryex..py
+++ Assignments/Dictionaryex..py
@@ -4,3 +4,11 @@ print(userDetails)
 
 location = dict(s = 'Samtse', t = 'Thimphu', p = 'Paro')
 print(location)
+
+print(userDetails['userName']) #Just_me
+print(location.get('t')) #Thimphu  = is also known as assignment operator
+
+userDetails['email'] = 'justme@example.com'
+print(userDetails)
+userDetails['userName'] = 'Just_me_updated'
+print(userDetails)
