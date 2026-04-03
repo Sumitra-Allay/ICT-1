2c85c42880ec8deb9a39064d43fa625038e1b92b
unknown <08250286.sce@rub.edu.bt>
2026-04-03 14:49:54 +0530

    Learning tuple

---
+++ Assignments/Tupleassiex.py
@@ -0,0 +1,7 @@
+my_tuple = ("hellow")
+result_tuple = (my_tuple[-1],my_tuple[0])
+print(result_tuple)
+
+my_tuple = ("H","e","l","l","o","w")
+result_tuple = (my_tuple[-1],my_tuple[0])
+print(result_tuple)
