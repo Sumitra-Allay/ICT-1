5ffb74c42538add3e5d6c486209c2cf6212c1f73
unknown <08250286.sce@rub.edu.bt>
2026-04-08 10:10:16 +0530

    Learning to do library Manager

---
+++ Assignments/LibraryManager.py
@@ -0,0 +1,43 @@
+books_list = []
+authors_set = set()
+books_dict = {}
+
+books_list.append("Python Programming")
+authors_set.add("John Smith")
+books_dict["Python Programming"]="John Smith"
+books_list.append("Python Fundamentals")
+authors_set.add("John Smith")
+books_dict["Python Fundamentals"] = "John Smith"
+
+books_list.append("Data Structures and algorithms")
+authors_set.add("Jane Doe")
+books_dict["Data Structures and algorithms"] = "Jane Doe"
+
+books_list.append("Machine Laerning Basics")
+authors_set.add("Alice Johnson")
+books_dict["Machine Learning Basics"] = "Alice Johnson"
+
+search_title = input("Enter the title of the book to search:")
+if search_title in books_list:
+    print(f"Book found!The Author of the books{search_title} is {books_dict[search_title]}")
+else:
+    print("Book not found!")
+
+remove_title = input ("Enter the title of the book to remove or else enter to skip:")
+if remove_title in books_list:
+
+    remove_author = books_dict[remove_title]
+    books_list.remove(remove_title)
+    del books_dict[remove_title]
+
+    if remove_author not in books_dict.values():
+       authors_set.remove(remove_author)
+
+    print("Book removed successfully!")
+    print("Books available along with their authors:", books_dict)
+    print("Just available books:", books_list)
+    print("Just available authors:",authors_set)
+
+else:
+    print("Books not found")    
+
