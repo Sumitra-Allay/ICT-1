951b253dcc37d59f206cdd79b7f04bddebf4c934
unknown <08250286.sce@rub.edu.bt>
2026-04-09 16:50:49 +0530

    Learning Iterating over list

--- unit 3/Iteratingoverlist.ex.py
+++ unit 3/Iteratingoverlist.ex.py
@@ -1,3 +1,20 @@
 li = ["Python Programming","Python Fundamental","Python Interview Questions"]
 for x in li:
     print (x)
+lenli = len(li) #len()returns the number of items in the list li i.e.3. This value is stored in the variable lenli
+for x in range(lenli): #x is a variable that takes the value of each index
+    print(li[x])
+
+new_tuple = tuple (li)
+for x in new_tuple:
+    print(x)
+lennew_tuple = len(new_tuple)
+for x in range(lennew_tuple):
+    print(new_tuple[0])
+
+new_set = set (li)
+for x in new_set:
+    print(x)
+
+
+
