a13d9b947da5b8be312ee01fe30f94d37bea78b5
unknown <08250286.sce@rub.edu.bt>
2026-04-10 15:44:14 +0530

    Learning nested loop

---
+++ unit 3/NestedLoopex.py
@@ -0,0 +1,3 @@
+for i in range(1,4) : #outer loop iteration from 1 to 3
+    for j in range(i): #inner loop iterates from 0 to i-i
+        print(f"Outer Loop iteration {i}, inner loop iteration {j+1}") 
