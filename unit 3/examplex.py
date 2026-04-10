d7ea9f0d49a8e0e7c1dcd31b2b2a27c519310d1f
unknown <08250286.sce@rub.edu.bt>
2026-04-10 15:36:15 +0530

    Learning for loop

--- unit 3/examplex.py
+++ unit 3/examplex.py
@@ -5,3 +5,7 @@ for x in tup:
 set1 = {10,30,20}
 for x in set1:
     print(x)
+
+BookDetails = dict({"Python Programming": "John Smith","Python Fundamentals":"Alice Johnson", "Python Interview Questions":"Jane Doe"})
+for keys in BookDetails:
+    print(keys,BookDetails[keys]) 
