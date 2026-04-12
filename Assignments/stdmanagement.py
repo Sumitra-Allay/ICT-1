230ff5c1622b680a1fbf96a5a5b53657d2fe8354
unknown <08250286.sce@rub.edu.bt>
2026-04-12 11:06:52 +0530

    Student information management system

--- Assignments/stdmanagement.py
+++ Assignments/stdmanagement.py
@@ -9,15 +9,15 @@ grade=input("Enter grade: ") #Enter the grade of the student
 
 #check in if studen exit and add the student name
 if name in student_data: #Checking the name of the student exist
-    print("Student already exists!") #Print "Student already exists" if name of student is the student list
+    print("Student already exists.") #Print "Student already exists" if name of student is the student list
 else:
     students.append(name) #Add the name
     student_data[name]={"age": age, "grade": grade} #add grade and age of the student
-    print("Student added successfully") #print "Student added successfully"
+    print("Student added.") #print "Student added"
 
 # displaying all students and their details
 if (student_data): #to check in the details of the student
-    print("No students found.") #print "Sorry,No students found." if student not found by the name
+    print("No students found.") #print "No students found." if student not found by the name
 else:
     for name in student_data: #Print the students details
         print("Name:", name) # Enter the name of the students 
@@ -33,14 +33,14 @@ if search in student_data:
     print("Age:", student_data[search]["age"]) #Enter age of the of the student
     print("Grade:", student_data[search]["grade"]) #Enter of the student
 else:
-    print("Student not found.") #Print "Sorry,Student not found." if student not found
+    print("Student not found.") #Print "Student not found." if student not found
 
 # remove student 
 remove_name = input("Enter name of the student to remove: ") #Enter name of the student to remove
 
 if remove_name in student_data: 
     students.remove(remove_name) #remove the student of the name
-    del student_data[remove_name] #Permanently removing the student from student data (student dict)
-    print("Student removed successfully.") #Print "Student removed successfully." after removing it
+    del student_data[remove_name] #removing the student from student data (student dict)
+    print("Student removed.") #Print "Student removed." after removing it
 else:
     print("Student not found.") #Display "Student not found." after removing 
