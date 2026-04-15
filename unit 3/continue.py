56936de50300911432d2b5a5914c172b8c2ea863
unknown <08250286.sce@rub.edu.bt>
2026-04-15 09:47:26 +0530

    Learning continue uner while loop

---
+++ unit 3/continue.py
@@ -0,0 +1,5 @@
+for i in range(4): 
+    if i==2:
+       continue #the continue statement is used to skip the current iteration of the loop when the value os i is equal to 2 and move to the next iteration.
+    print(i)
+print("Loop ended") 
