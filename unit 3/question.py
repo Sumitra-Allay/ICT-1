1db37f5ff00e68ec2ddd98d3aa81a2b139a97f33
unknown <08250286.sce@rub.edu.bt>
2026-04-15 10:03:43 +0530

    Setting countdown from 10 to 1

---
+++ unit 3/question.py
@@ -0,0 +1,12 @@
+#Countdown Timer :Write the program using while loop that prints numbers from 10 down to 1, and the prints "Time's up'"
+
+#initialize countdown start
+count = 10
+
+#countdown loop
+while count >= 1:
+    print (count)
+    count -= 1 #It decreases from 10 by 1
+
+#Final message
+print("Time's up")  
